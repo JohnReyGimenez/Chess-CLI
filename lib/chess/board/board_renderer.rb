9dcc15c30b5f030cf0d756c8d08e1a559041e059
JohnReyGimenez <gimenezjr5@gmail.com>
2025-04-05 21:46:28 +0800

    added square content function

--- lib/chess/board/board_renderer.rb
+++ lib/chess/board/board_renderer.rb
@@ -1,24 +1,31 @@
 # frozen_string_literal: true
 
+require_relative 'board_general'
+
 module Chess
   class BoardRenderer
     SQUARE = '[ ]'
-    attr_reader :square_order
+    attr_reader :square_order, :board
 
-    def initialize(square_order)
+    def initialize(board)
+      @board = board
       @square_order = square_order
     end
 
     def render
       square_order.times do |_row|
         square_order.times do |column|
-          column != 7 ? print(SQUARE) : print(SQUARE)
+          column != 7 ? print(SQUARE) : puts(SQUARE)
         end
       end
+    end
+
+    def square_content(square)
+      "[#{board[square.class.to_s[0]]}]"
     end
   end
 end
 
-renderer = BoardRenderer.new(8)
+renderer = Chess::BoardRenderer.new(board)
 
 renderer.render
