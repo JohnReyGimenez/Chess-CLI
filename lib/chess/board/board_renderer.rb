4f2f5ce1df8b288c8134ce8ed5bf8fabbc3d091d
JohnReyGimenez <gimenezjr5@gmail.com>
2025-04-05 21:21:18 +0800

    added render function

--- lib/chess/board/board_renderer.rb
+++ lib/chess/board/board_renderer.rb
@@ -1,10 +1,24 @@
+# frozen_string_literal: true
+
 module Chess
-  class BoardRendered
+  class BoardRenderer
     SQUARE = '[ ]'
     attr_reader :square_order
 
     def initialize(square_order)
       @square_order = square_order
     end
+
+    def render
+      square_order.times do |_row|
+        square_order.times do |column|
+          column != 7 ? print(SQUARE) : print(SQUARE)
+        end
+      end
+    end
   end
 end
+
+renderer = BoardRenderer.new(8)
+
+renderer.render
