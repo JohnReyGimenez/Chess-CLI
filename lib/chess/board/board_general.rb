f5d6ce0cdde1a9217372f796fd2bad21fa5c2106
JohnReyGimenez <gimenezjr5@gmail.com>
2025-04-19 20:51:44 +0800

    fix: turned intial setup to hash. added set up board function.

--- lib/chess/board/board_general.rb
+++ lib/chess/board/board_general.rb
@@ -5,12 +5,17 @@ require 'colorize'
 module Chess
   class Board # rubocop:disable Style/Documentation
     SQUARE_ORDER = 8
-    INITIAL_SETUP = [
-      [Rook, 0, 0], [Knight, 0, 1], [Bishop, 0, 2], [Queen, 0, 3],
-      [King, 0, 4], [Bishop, 0, 5], [Knight, 0, 6], [Rook, 0, 7],
-      [Rook, 7, 0], [Knight, 7, 1], [Bishop, 7, 2], [Queen, 7, 3],
-      [King, 7, 4], [Bishop, 7, 5], [Knight, 7, 6], [Rook,  7, 7]
-    ].freeze
+    # format: [piece_class, row, col]
+    INITIAL_SETUP = {
+      black: [
+        [Rook, 0, 0], [Knight, 0, 1], [Bishop, 0, 2], [Queen, 0, 3],
+        [King, 0, 4], [Bishop, 0, 5], [Knight, 0, 6], [Rook, 0, 7]
+      ],
+      white: [
+        [Rook, 7, 0], [Knight, 7, 1], [Bishop, 7, 2], [Queen, 7, 3],
+        [King, 7, 4], [Bishop, 7, 5], [Knight, 7, 6], [Rook, 7, 7]
+      ]
+    }.freeze
 
     attr_reader :grid
 
@@ -28,6 +33,21 @@ module Chess
     def []=(square, piece)
       row, column = square
       grid[row][column] = piece
+    end
+
+    def set_up_board
+      # format: [piece_class, row, col]
+      INITIAL_SETUP.each do |color, pieces|
+        pieces.each do |piece_class, row, column|
+          place_pieces(piece_class, row, column, color)
+        end
+      end
+
+      # places pawns
+      8.times do |col|
+        place_piece(Pawn, 1, col, :black)
+        place_piece(Pawn, 6, col, :white)
+      end
     end
   end
 end
