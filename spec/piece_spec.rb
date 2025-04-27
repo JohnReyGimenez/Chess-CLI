ab0adea0ed2664134a6a901376876d3cba9d86b9
JohnReyGimenez <gimenezjr5@gmail.com>
2025-04-27 11:18:54 +0800

    completed rspec test for initialized constant in piece class

--- spec/piece_spec.rb
+++ spec/piece_spec.rb
@@ -3,8 +3,11 @@ require_relative '../lib/chess/chess_pieces'
 
 describe Piece do
   describe '#initialize' do
-    it 'initializes the instance variables: location, color, has_moved'
-    piece = Chess::Piece.new
-    expect(piece)
+    it 'initializes the instance variables: location, color, has_moved' do
+      piece = Chess::Piece.new([0, 0], :white, False)
+      expect(piece.location).to eq([0, 0])
+      expect(piece.color).to eq(:white)
+      expect(piece.has_moved).to eq(false)
+    end
   end
 end
