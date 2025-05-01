81ca199079588b98d07bc5c2b14e5f8c960471de
JohnReyGimenez <gimenezjr5@gmail.com>
2025-05-01 18:46:40 +0800

    added direction constant and draft for forward moves function

--- lib/chess/chess_pieces/pawn.rb
+++ lib/chess/chess_pieces/pawn.rb
@@ -5,9 +5,15 @@ module Chess
   class Pawn < Chess::Piece
     WHITE = '♟'.colorize(color: :blue)
     BLACK = '♟'.colorize(color: :black)
+    DIRECTION = color == :white ? -1 : 1
   end
 
-  def valid_move
+  def valid_move(board)
+    valid_moves = []
     # sub
+  end
+
+  def forward_moves(board)
+    next_row = 
   end
 end
