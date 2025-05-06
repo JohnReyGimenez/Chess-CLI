b56ea9321879310fba1987bb3065ea729da29355
JohnReyGimenez <gimenezjr5@gmail.com>
2025-05-06 20:29:48 +0800

    added draft valid moves testing for bishop

--- spec/bishop_spec.rb
+++ spec/bishop_spec.rb
@@ -12,4 +12,15 @@ RSpec.describe Chess::Bishop do
       expect(piece.color).to eq(:white)
     end
   end
+
+  describe '#valid_moves' do
+    it 'returns the valid and capture moves from current position' do
+      board = double('board')
+      allow(board).to receive(:in_bounds?).and_return(true)
+      allow(board).to receive(:[]).and_return(nil) # simulate empty squares
+
+      piece = Chess::Bishop.new([0, 2], :white)
+      expect(piece.valid_moves(board)).to include([1, 2], [3, 0])
+    end
+  end
 end
