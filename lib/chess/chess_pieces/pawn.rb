68697d42fcd64f9f555780d9c7e827f3608b5326
JohnReyGimenez <gimenezjr5@gmail.com>
2025-05-07 22:04:20 +0800

    fix: moved valid_moves inside class, removed redundant forward_moves

--- lib/chess/chess_pieces/pawn.rb
+++ lib/chess/chess_pieces/pawn.rb
@@ -5,34 +5,29 @@ module Chess
   class Pawn < Chess::Piece
     WHITE = '♟'.colorize(color: :blue)
     BLACK = '♟'.colorize(color: :black)
-  end
 
-  def valid_moves(board)
-    row, col = @location
-    direction = color == :white ? -1 : 1 # direction
-    moves = []
-
-    # 1-step
-    one_step = [row + direction, col]
-    moves << one_step if board[one_step[0]][one_step[1]].nil?
-
-    # 2-step
-    two_step = [row + 2 * direction, col]
-    moves << two_step if !@has_moved && board[one_step[0]][one_step[1]].nil? && board[two_step[0]][two_step[1]].nil?
-
-    # Diagonal captures
-    [[row + direction, col - 1], [row + direction, col + 1]].each do |r, c|
-      next unless board.in_bounds?([r, c])
-
-      target = board[r][c]
-      moves << [r, c] if target && target.color != color
+    def valid_moves(board)
+      row, col = @location
+      direction = color == :white ? -1 : 1 # direction
+      moves = []
+  
+      # 1-step
+      one_step = [row + direction, col]
+      moves << one_step if board[one_step[0]][one_step[1]].nil?
+  
+      # 2-step
+      two_step = [row + 2 * direction, col]
+      moves << two_step if !@has_moved && board[one_step[0]][one_step[1]].nil? && board[two_step[0]][two_step[1]].nil?
+  
+      # Diagonal captures
+      [[row + direction, col - 1], [row + direction, col + 1]].each do |r, c|
+        next unless board.in_bounds?([r, c])
+  
+        target = board[r][c]
+        moves << [r, c] if target && target.color != color
+      end
+  
+      moves
     end
-
-    moves
-  end
-
-  def forward_moves(board)
-    next_row = current_row + DIRECTION
-    valid_moves << [next_row, current_col] if board[[next_row, current_col]].nil
   end
 end
