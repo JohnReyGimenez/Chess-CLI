05688f5285e15199d5f05b82d8f67ad6a006e928
JohnReyGimenez <gimenezjr5@gmail.com>
2025-05-09 22:42:15 +0800

    added: legal_moves function in bishop.rb

--- lib/chess/chess_pieces/bishop.rb
+++ lib/chess/chess_pieces/bishop.rb
@@ -5,10 +5,41 @@ module Chess
   class Bishop < Chess::Piece
     WHITE = '♝'.colorize(color: :blue)
     BLACK = '♝'.colorize(color: :black)
-  end
 
-  def legal_moves
-    # sub
+    def valid_moves(board)
+      row, col = @location
+
+      # All 4 diagonal directions
+      directions = [
+        [-1, -1], [-1, 1],
+        [1, -1],  [1, 1]
+      ]
+
+      moves = []
+
+      directions.each do |dr, dc|
+        new_row = row + dr
+        new_col = col + dc
+
+        while board.in_bounds?([new_row, new_col])
+          target = board[new_row][new_col]
+
+          if target.nil? # if square is empty
+            moves << [new_row, new_col]
+          elsif target.color != color # if enemy valid but brekas when capture
+            moves << [new_row, new_col]
+            break
+          else
+            break # if friendly break
+          end
+
+          new_row += dr
+          new_col += dc
+        end
+      end
+
+      moves
+    end
   end
 end
 
