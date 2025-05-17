43fddb1a93f5c470bfb7423c4db3fd5dcdba191d
JohnReyGimenez <gimenezjr5@gmail.com>
2025-05-17 20:02:51 +0800

    added: expected valid moves for queen.rb rpsec testing

--- spec/queen_spec.rb
+++ spec/queen_spec.rb
@@ -19,7 +19,17 @@ RSpec.describe Chess::Queen do
       queen = Chess::Queen.new([3, 3], :white)
       board[[3, 3]] = queen
 
-      expected_moves = []
+      expected_moves = [
+        # Horizontal
+        [3, 0], [3, 1], [3, 2], [3, 4], [3, 5], [3, 6], [3, 7],
+        # Vertical
+        [0, 3], [1, 3], [2, 3], [4, 3], [5, 3], [6, 3], [7, 3],
+        # Diagonals
+        [2, 2], [1, 1], [0, 0],
+        [2, 4], [1, 5], [0, 6],
+        [4, 2], [5, 1], [6, 0],
+        [4, 4], [5, 5], [6, 6], [7, 7]
+      ]
 
       expect(queen.valid_moves(board)).to match_array(expected_moves)
     end
