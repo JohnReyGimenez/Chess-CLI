e3fadc4f06250f642f08f7e5e88467b2a866057d
JohnReyGimenez <gimenezjr5@gmail.com>
2025-05-19 22:57:34 +0800

    initialized game.rb

--- lib/chess/game.rb
+++ lib/chess/game.rb
@@ -1,8 +1,13 @@
 # frozen_string_literal: true
 
 module Chess
-  class Game
-    def initialize; end
+  class Game # rubocop:disable Style/Documentation
+    def initialize
+      @board = Board.new
+      @board.set_up_board
+      @renderer = BoardRenderer.new(@board)
+      @current_player_color = :white
+    end
 
     def play_game; end
   end
