57940e6eb01130f7f295b8b52bcfc35091accf0f
JohnReyGimenez <gimenezjr5@gmail.com>
2025-05-19 23:04:53 +0800

    added: get_move function on player_input.rb

--- lib/chess/player_input.rb
+++ lib/chess/player_input.rb
@@ -6,5 +6,16 @@ module Chess
     def initialize
       @board = board
     end
+
+    def self.get_move
+      loop do
+        print '> '
+        input = gets.chomp.strip.downcase
+
+        return input if valid_move_input?(input)
+
+        puts "Invalid input format. Try 'e2e4' or 'castle k'."
+      end
+    end
   end
 end
