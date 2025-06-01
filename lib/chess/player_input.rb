d36ecc5feca7d7a14396b66c10d83d74f1dd63ab
JohnReyGimenez <gimenezjr5@gmail.com>
2025-06-01 13:00:12 +0800

    moved valid_move_input to play_input,rb

--- lib/chess/player_input.rb
+++ lib/chess/player_input.rb
@@ -9,7 +9,20 @@ module Chess
       @board = board
     end
 
-    def self.get_move
+    def valid_move_input?(input)
+      input = input.downcase.strip
+      return true if input == 'exit'
+      return true if ['castle k', 'castle q'].include?(input)
+      return false unless input.length == 4
+
+      from = input[0..1]
+      to = input[2..3]
+      valid_positions = ('a'..'h').to_a.product((1..8).to_a).map { |l, n| "#{l}#{n}" }
+
+      valid_positions.include?(from) && valid_positions.include?(to)
+    end
+
+    def get_move
       loop do
         print "Enter your move (e.g., 'e2 e4') or 'save': "
         input = gets.chomp.strip.downcase
