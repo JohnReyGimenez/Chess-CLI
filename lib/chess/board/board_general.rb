518041baf4896156972123c2442d4bfda198e73d
JohnReyGimenez <gimenezjr5@gmail.com>
2025-06-08 20:30:43 +0800

    draft fix for en passant not being captured

--- lib/chess/board/board_general.rb
+++ lib/chess/board/board_general.rb
@@ -79,31 +79,29 @@ module Chess
     def move_piece_to(from, to)
       piece = self[from]
 
-      # reset en passant target by default
-      @en_passant_target = nil
+      # detect en passant before making the move
+      en_passant_capture = piece.is_a?(Pawn) && to == @en_passant_target && self[to].nil?
 
-      # track en passant capture before move
-      en_passant_capture = false
-      en_passant_capture = true if piece.is_a?(Pawn) && to == @en_passant_target && self[to].nil?
-
-      # move piece normally
+      # move the piece
       self[to] = piece
       self[from] = nil
       piece.location = to
       piece.has_moved = true
 
-      # set en passant target if double move
+      # remove captured pawn if en passant
+      if en_passant_capture
+        captured_pawn_row = from[0]
+        captured_pawn_col = to[1]
+        self[[captured_pawn_row, captured_pawn_col]] = nil
+      end
+
+      # set en passant target square
       if piece.is_a?(Pawn) && (from[0] - to[0]).abs == 2
         direction = piece.color == :white ? -1 : 1
         @en_passant_target = [from[0] + direction, from[1]]
+      else
+        @en_passant_target = nil
       end
-
-      # remove the captured pawn if it was an en passant move
-      return unless en_passant_capture
-
-      captured_pawn_row = from[0]
-      captured_pawn_col = to[1]
-      self[[captured_pawn_row, captured_pawn_col]] = nil
     end
 
     def board_dup
